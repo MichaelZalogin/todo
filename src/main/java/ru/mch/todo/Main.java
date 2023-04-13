0cad0c113e4c7e9f1652f8d9684fd00408b77490
MichaelZalogin <zaloginmiha@gmail.com>
2023-04-14 01:22:34 +0300

    init repository

---
+++ src/main/java/ru/mch/todo/Main.java
@@ -0,0 +1,20 @@
+package ru.mch.todo;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.context.annotation.Bean;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Bean(destroyMethod = "close")
+    public SessionFactory sf() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+}
---
+++ src/main/java/ru/mch/todo/repository/TaskStore.java
@@ -0,0 +1,11 @@
+package ru.mch.todo.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@AllArgsConstructor
+public class TaskStore {
+    private final SessionFactory sf;
+}
