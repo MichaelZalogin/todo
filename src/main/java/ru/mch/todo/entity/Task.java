cb0a621e6f85039d7987d00caba3a34b51e954b3
MichaelZalogin <zaloginmiha@gmail.com>
2023-04-16 23:38:11 +0300

    Add entity

---
+++ src/main/java/ru/mch/todo/entity/Task.java
@@ -0,0 +1,29 @@
+package ru.mch.todo.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Column(name = "task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created")
+    private LocalDateTime created = LocalDateTime.now();
+
+    @Column(name = "done")
+    private boolean done;
+
+}
