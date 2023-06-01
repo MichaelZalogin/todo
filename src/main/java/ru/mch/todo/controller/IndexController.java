2a8325175a90d1357eb996bf9061224d3bb00c65
MichaelZalogin <zaloginmiha@gmail.com>
2023-06-01 23:41:29 +0300

    Добавил jsip, travis CI [#331992]

--- src/main/java/ru/mch/todo/controller/IndexController.java
+++ src/main/java/ru/mch/todo/controller/IndexController.java
@@ -1,12 +1,12 @@
 package ru.mch.todo.controller;
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping()
+@ThreadSafe
 public class IndexController {
 
     @GetMapping({"/", "/index"})
